2a2dbbd71fb7501ff1fc20f75319007a77fb70c9
Mariya040122 <97065409+Mariya040122@users.noreply.github.com>
2022-12-05 02:28:26 +0300

    Update CommentDto.java

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -1,9 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
