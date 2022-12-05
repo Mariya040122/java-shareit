f0f12688a2946d2ff18665130ef3010609f6d4de
Mariya040122 <97065409+Mariya040122@users.noreply.github.com>
2022-12-05 21:37:21 +0300

    Update ItemWithBookingDTO.java

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemWithBookingDTO.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemWithBookingDTO.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.practicum.shareit.booking.dto.BookingDto;
 
@@ -10,8 +8,6 @@ import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class ItemWithBookingDTO {
 
     Long id;  //уникальный идентификатор вещи
